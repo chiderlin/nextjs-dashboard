d365ab932541cdfa50d774c8dc5b1ddef47b4164
chi <chiderlin36@gmail.com>
2024-04-26 00:02:35 +0900

    done chapter4. creating layouts and pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
+// any UI you add to the root layout will be shared across all pages in your app.
 export default function RootLayout({
   children,
 }: {
